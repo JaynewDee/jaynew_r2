a2985e4bcc0f23fd568edb48c5c39ff68c319f85
NuminousBlue <jdiehl2236@gmail.com>
2022-02-05 21:45:07 -0600

    reset readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+export const App: React.FC = () => {
+   return (
+      <div>
+         App running
+      </div>
+   )
 }
 
-export default App;
+
