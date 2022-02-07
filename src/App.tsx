06546a67111be290226363fc1f4eef1f80175cbb
NuminousBlue <jdiehl2236@gmail.com>
2022-02-06 21:52:23 -0600

    using typescript interfaces building functional react components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Main } from './pages/Main'
+
 
 export const App: React.FC = () => {
    return (
       <div>
-         App running
+         <Main></Main>
       </div>
    )
 }
---
+++ src/pages/Main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StartButton} from '../components/startButton'
+import CSS from 'csstype';
+
+
+
+export const Main: React.FC = () => {
+  return(
+      <div>
+         <StartButton></StartButton>
+      </div>
+  )
+};
+
+
+
+
