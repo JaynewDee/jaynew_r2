4ac21b2da5ba21e244858fe6210fd2465fb42a2b
NuminousBlue <jdiehl2236@gmail.com>
2022-02-09 14:43:39 -0600

    exploring the use of typescript alongside react components

--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
-export const StartButton = () => {
-  return <div>
-     <Button></Button>
-  </div>;
+const btnStyles = {
+   margin: 'auto',
+   height: '100px',
+   width: '150px',
+   backgroundColor: 'black',
+   color: 'white',
+   fontSize: '20px',
+   borderRadius: '20px'
+}
+
+interface btnType {
+   id: number;
+   style?: {};
+}
+
+export const StartButton: React.FC<btnType> = () => {
+  return(  
+  
+  <div>
+     <Button style={btnStyles}>ENTER</Button>
+  </div>
+  )
 };
 
 
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import {StartButton} from '../components/startButton'
 import CSS from 'csstype';
 
+const mainContStyles = {
+   display: 'flex',
+}
+
+
 
 
 export const Main: React.FC = () => {
+
   return(
+   <>
       <div>
-         <StartButton></StartButton>
+         <StartButton style={mainContStyles} id={1}></StartButton>
       </div>
+   </>
   )
 };
 
